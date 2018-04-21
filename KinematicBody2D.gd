562954c345100fa5f6a5aeec9afc2a316e41a5ec
River Merrill <rivermerrill.me@gmail.com>
2018-04-21 14:51:44 -0600

    Added jumping to script and some floor to jump off of

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,15 +1,22 @@
 extends KinematicBody2D
 
+const UP = Vector2(0,-1)
+
 var motion = Vector2()
 
 func _process(delta):
-	motion.y = 30
+	motion.y += 20
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = 250
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -250
 	else:
 		motion.x = 0
-	move_and_slide(motion)
+	if is_on_floor():
+		motion.y = 20
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -600
+	motion = move_and_slide(motion, UP)
+	pass
 
 
